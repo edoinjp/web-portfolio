0daa86f1e3b0a908742d84bf289fd5e25dc26a68
eduard gurchiani <edoinjp@gmail.com>
2024-03-13 23:00:14 +0900

    styling main content of page

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+  var jobTitle = document.getElementById('jobTitle');
+  var newText = "front end developer";
+  var currentText = jobTitle.innerText;
+
+  function replaceText(index) {
+    if (index >= 0) {
+      jobTitle.innerText = currentText.substring(0, currentText.length - index - 1) + newText.charAt(newText.length - index - 1);
+      setTimeout(function() {
+        replaceText(index - 1);
+      }, 50); // Adjust this value to control the speed of typing effect
+    } else {
+      setTimeout(function() {
+        replaceOriginalText(newText.length - 1);
+      }, 3000); // Delay before reverting back
+    }
+  }
+
+  function replaceOriginalText(index) {
+    if (index >= 0) {
+      jobTitle.innerText = newText.substring(0, newText.length - index - 1) + currentText.charAt(currentText.length - index - 1);
+      setTimeout(function() {
+        replaceOriginalText(index - 1);
+      }, 50); // Adjust this value to control the speed of typing effect
+    } else {
+      setTimeout(function() {
+        replaceText(newText.length - 1);
+      }, 3000); // Delay before starting next animation
+    }
+  }
+
+  replaceText(newText.length - 1); // Start the animation immediately
+};
